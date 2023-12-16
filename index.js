64b7af2356afe27444245c3e3d845fd9bd0ed4c3
grexgi <79434596+ReggiFachri@users.noreply.github.com>
2023-12-16 07:36:45 +0700

    add Rest API

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
-import app from './app.js';
-import {logger, initLogCorrelation} from './utils/logging.js';
-import {fetchProjectId} from './utils/metadata.js';
-
-/**
- * Initialize app and start Express server
- */
-const main = async () => {
-  let project = process.env.GOOGLE_CLOUD_PROJECT;
-  if (!project) {
-    try {
-      project = await fetchProjectId();
-    } catch (err) {
-      logger.warn('Could not fetch Project Id for tracing.');
-    }
-  }
-  // Initialize request-based logger with project Id
-  initLogCorrelation(project);
-
-  // Start server listening on PORT env var
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
-};
-
-/**
- * Listen for termination signal
- */
-process.on('SIGTERM', () => {
-  // Clean up resources on shutdown
-  logger.info('Caught SIGTERM.');
-  logger.flush();
+app.use(express.json());
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`API listening on port ${port}`);
 });
 
-main();
+app.get("/", async (req, res) => {
+  res.json({ status: "Ready!!" });
+});
+
+app.get("/:batik", async (req, res) => {
+  const query = "SELECT * FROM data_batik WHERE nama_batik = ?";
+  pool.query(query, [req.params.batik], (error, results) => {
+    if (!results[0]) {
+      res.json({ status: "Not Found!" })
+    } else {
+      res.json(results[0])
+    }
+  })
+});
+
+const pool = mysql.createPool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.DB_USER}`,
+});
